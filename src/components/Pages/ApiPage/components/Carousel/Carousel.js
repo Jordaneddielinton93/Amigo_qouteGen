06b133406c3af58a9e22f5114358d40c513112e5
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-10-08 19:49:01 +0100

    instead of using 10 harder qoutes i used the customhook api usefetch

--- src/components/Pages/ApiPage/components/Carousel/Carousel.js
+++ src/components/Pages/ApiPage/components/Carousel/Carousel.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { CarouselStyle } from "./Carousel.style";
-
+import useFetch from "../../../../useFetch/useFetch"
 const Carousel = () => {
 
   let [posCounter,setPosCounter] = useState(-1200)
@@ -15,42 +15,21 @@ const Carousel = () => {
 
   },[posCounter])
 
-
+  let {textArr,setTextArr}= useFetch("https://type.fit/api/quotes")
+console.log(textArr)
 
   return ( 
     <CarouselStyle>
       <div className="CarouselConatiner">
 
         <main className="CarouselConatiner__Slider" style={{left:posCounter}}>
-          <p className="CarouselConatiner__Slider--Quotes">
-          “Ever tried. ...”
-          </p>
-          <p className="CarouselConatiner__Slider--Quotes">
-          “Only I can change my life. ..”
-          </p>
-          <p className="CarouselConatiner__Slider--Quotes">
-          “Start where you are. ...”
-          </p>
-          <p className="CarouselConatiner__Slider--Quotes">
-          “If you can dream it <br />, you can do it”
-          </p>
-          <p className="CarouselConatiner__Slider--Quotes">
-          “The purpose of our lives is to be happy.”
-          </p>
-          <p className="CarouselConatiner__Slider--Quotes">
-          “Life is what happens when you’re busy making other plans.”
-          </p>
-          <p className="CarouselConatiner__Slider--Quotes">
-          “You only live once, but if you do it right, once is enough.”
-          </p>
-          <p className="CarouselConatiner__Slider--Quotes">
-          “Many of life’s failures are people who did not realize how close they were to success when they gave up.”
-          </p>
-          <p className="CarouselConatiner__Slider--Quotes">
-          “Money and success don’t change people; they merely amplify what is already there.”
-          </p>
-          
-
+          {textArr.map((obj)=>{
+            return (
+              <p className="CarouselConatiner__Slider--Quotes">
+                  “{obj.text}”
+              </p>
+            )
+          })}
         </main>
 
       </div>
